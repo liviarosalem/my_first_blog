10e3a3735d8a06275fab7a6551a5d1a563e86854
livia <liviarosalem@gmail.com>
2019-06-07 14:35:31 -0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+
+def oi(nome):
+    print("Olá " + nome + "!")    
+
+oi("Sonja")
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'livia']
+
+for name in girls:
+    oi(name)
+    print("PRÓXIMA")
+
